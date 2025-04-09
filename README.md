8436c95c6fb8d7502e86f7ad9706876ff75bdbf6
Sunil Hanamshetty <64653515+shetty456@users.noreply.github.com>
2025-04-09 07:55:13 +0530

    added readme and change settings.py

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # MovieBuzz Backend 
+# install required dependences-'pip install --no-cache-dir -r requirements.txt'
+# build docker image - 'docker build -t moviebuzz-backend .'
+
+
+# run docker file - 'docker run -d -p 8000:8000 moviebuzz'
+
+# if u are using docker compose - 'docker-compose up'
+# stop docker compose - 'docker-compose down'
+"using sqlite3 as a database"
+
+"the backend is crreated by using django and DRF"
+
+
+
--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'drf_spectacular'
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'config.urls'
 
@@ -125,9 +128,13 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # DRF & JWT Configuration
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ),
+    "DEFAULT_SCHEMA_CLASS":"drf_spectacular.apenapi.AutoSchema",
+    "DEFAULT_RENDERER_CLASSES": [
+        "rest_framework.renderers.JSONRender",
+    ],
+    "DEFAULT_PARSER_CLASSES": [
+        "rest_framework.parser.JSONparser",
+    ],
 }
 
 SIMPLE_JWT = {
