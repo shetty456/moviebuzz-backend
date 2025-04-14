ae203bfd815988ad7168c31dc763939e9ed6576c
jrjagdish <hanamshettyjagdish1@gmail.com>
2025-04-14 19:49:01 +0530

    conflicts resolved

--- user/permissions.py
+++ user/permissions.py
@@ -5,7 +5,7 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-class IsAdmin(BasePermission):
+class IsAdminorReadonly(BasePermission):
 
     def has_permission(self, request, view):
         return request.user.is_authenticated and request.user.Is_Admin_role
